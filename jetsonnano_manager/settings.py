8da9ce9b47d11d4770fb9eb6c79bb64024a152b5
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2025-10-28 19:36:05 +0100

    fix: update ALLOWED_HOSTS in settings.py

--- jetsonnano_manager/settings.py
+++ jetsonnano_manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-=414*!4+jk0-pi_ne#7iglt!dnzx=tybedw&)w@1!(6n3t!)j2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["192.168.137.17", "localhost", "127.0.0.1"]
 
 
 # Application definition
